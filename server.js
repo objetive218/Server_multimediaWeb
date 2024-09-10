290d48781cad4262e52d687296b55318c4ae0ee1
objetive218 <davidsantiagotc@gmail.com>
2024-09-09 23:50:12 -0500

    create get login to check the user and password

--- server.js
+++ server.js
@@ -15,29 +15,42 @@ app.use(express.json());
 connectDb();
 
 app.get("/people", async (req, res) => {
-    const people = await Person.find();
-    if(res){
-        res.json({people: people});
-    }else {
-        res.status(404).end();
-    }
-})
+  const people = await Person.find();
+  if (res) {
+    res.json({ people: people });
+  } else {
+    res.status(404).end();
+  }
+});
 
 app.post("/people", async (req, res) => {
-    const user = req.body.user;
-    const password = req.body.password;
-    const mail = req.body.mail;
-    const person = await Person.create({
-        user: user,
-        password: password,
-        mail:mail,
-    })
-    if(person){
-        res.json({person : person})
-    }else {
-        res.status(404).end();
-    }
-})
+  const user = req.body.user;
+  const password = req.body.password;
+  const mail = req.body.mail;
+  const person = await Person.create({
+    user: user,
+    password: password,
+    mail: mail,
+  });
+  if (person) {
+    res.json({ person: person });
+  } else {
+    res.status(404).end();
+  }
+});
 
+app.get("/login", async (req, res) => {
+  const userName = req.body.user;
+  const password = req.body.password;
+  const user = await Person.findOne({
+    user: userName,
+    password: password,
+  }).exec();
+  if (user) {
+    res.json(true);
+  } else {
+    res.status(404).end();
+  }
+});
 
 app.listen(process.env.PORT);
